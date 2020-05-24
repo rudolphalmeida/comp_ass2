4a832e4854b6656b0a04dad0354a284ec44fdca5
Rudolph Almeida <rudolf1.almeida@gmail.com>
2020-05-24 19:24:52 +1000

    Changed app structure again

--- harvester/main.py
+++ harvester/main.py
@@ -1,48 +1,85 @@
+import json
 import sys
 
-from harvesters import TwitterStreamer
-from listeners import CouchDBListener, ToFileListener
+from tweepy.streaming import StreamListener
+from tweepy import Stream
+import couchdb
+
+import credentials
 
 
-AUS_GEO_RANGE = [112, -44, 155, -10]
-GROUP_MEMBERS = ["RUD", "SAG", "SHE", "VIS", "SHA"]
+class ToFileListener(StreamListener):
+    """
+    A stream listener that just prints the tweets to file
+    """
+
+    def __init__(self, file, api=None):
+        self.file = file
+        super().__init__(api=api)
+
+    def on_data(self, raw_data):
+        print(raw_data, file=self.file)
+        return True
+
+    def on_error(self, status_code):
+        if status_code == 420:  # Rate limit reached
+            # Disconnect the stream
+            return False
 
 
-for member in GROUP_MEMBERS:
-    streamer = TwitterStreamer(
-        member,
-        # Hashtags and usernames to track
-        # FIXME: This is not honoured
-        # track=[
-        #     "MasterChefAU",
-        #     "masterchefaustralia",
-        #     "masterchef",
-        #     "SimonTohey",
-        #     "tastingaustralia",
-        #     "masterchef2020",
-        #     "season12",
-        #     "RoseAdamCooks",
-        #     "sarahclarecooks",
-        #     "S_Tiong",
-        #     "SimonToohey",
-        #     "TessaBoersma",
-        #     "trackycollins_",
-        #     "harry_fos",
-        #     "hayden_quinn",
-        #     "jesselemon",
-        #     "khanhong",
-        # ],
-        track=["trump"],
-        locations=AUS_GEO_RANGE,
+class CouchDBListener(StreamListener):
+    """
+    A stream listener that extracts relevant information from tweets and
+    stores them to the configured CouchDB instance
+    """
+
+    def __init__(self, server_id, api=None):
+        couch = couchdb.Server(server_id)
+        try:
+            self.db = couch.create("tweets")
+        except Exception:
+            self.db = couch["tweets"]  # Table is created
+        super().__init__(api=api)
+
+    def on_data(self, raw_data):
+        self.db.save(json.loads(raw_data))
+
+    def on_error(self, status_code):
+        if status_code == 420:  # Rate limit reached
+            # Disconnect the stream
+            return False
+
+
+def stream_tweets(auth, listener, track):
+    stream = Stream(auth, listener)
+    stream.filter(track=track)
+
+
+if __name__ == "__main__":
+    # AUS_GEO_RANGE = [112, -44, 155, -10]
+
+    # Authenticate using config.py and connect to Twitter Streaming API.
+    track = [
+        "#MasterChefAU",
+        "#masterchefaustralia",
+        "#masterchef",
+        "#SimonToohey",
+        "#tastingaustralia",
+        "#masterchef2020",
+        "#season12",
+        "@RoseAdamCooks",
+        "@sarahclarecooks",
+        "@S_Tiong",
+        "@SimonToohey",
+        "@TessaBoersma",
+        "@trackycollins_",
+        "@harry_fos",
+        "@hayden_quinn",
+        "@jesselemon",
+        "@khanhong",
+    ]
+    stream_tweets(
+        auth=credentials.authenticate("RUD"),
+        listener=ToFileListener(sys.stdout),
+        track=track,
     )
-    stream = None
-    try:
-        # stream = streamer.stream(
-        #     CouchDBListener("http://admin:password@127.0.0.1:5984/")
-        # )
-        stream = streamer.stream(ToFileListener(sys.stdout))
-    except KeyboardInterrupt:
-        print("Shutting down...")
-        if stream is not None:
-            stream.shutdown()
-        sys.exit(0)
